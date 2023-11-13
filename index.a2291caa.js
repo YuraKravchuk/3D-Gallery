58a8c4fb821d49d0e53ec4ea3a392c5b18435a65
YuraKravchuk <114563784+YuraKravchuk@users.noreply.github.com>
2023-11-13 15:01:15 +0000

    Deploying to gh-pages from @ YuraKravchuk/3D-Gallery@fb1958d575c44d0c3468aa74bdc320c73c1035cf 🚀

---
+++ index.a2291caa.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".prev"),t=document.querySelector(".next"),r=document.querySelector(".box");let c=0;e.addEventListener("click",(function(){c+=45,r.style=`transform: perspective(1000px) rotateY(${c}deg)`})),t.addEventListener("click",(function(){c-=45,r.style=`transform: perspective(1000px) rotateY(${c}deg)`}));
+//# sourceMappingURL=index.a2291caa.js.map
---
+++ index.b1e3ac3c.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".prev"),t=document.querySelector(".next"),c=document.querySelector(".box"),n=0;e.addEventListener("click",(function(){n+=45,c.style="transform: perspective(1000px) rotateY(".concat(n,"deg)")})),t.addEventListener("click",(function(){n-=45,c.style="transform: perspective(1000px) rotateY(".concat(n,"deg)")}))}();
+//# sourceMappingURL=index.b1e3ac3c.js.map
